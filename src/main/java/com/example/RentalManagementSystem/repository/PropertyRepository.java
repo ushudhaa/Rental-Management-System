914150d03701785a169e0b7230e8d25576932b43
Ushudha Sanwa Limbu <ushudhasanwa@gmail.com>
2026-07-23 10:59:12 +0545

    - Add PaymentRepository with findByPropertyId and findByStatus

--- src/main/java/com/example/RentalManagementSystem/repository/PropertyRepository.java
+++ src/main/java/com/example/RentalManagementSystem/repository/PropertyRepository.java
@@ -1,7 +1,12 @@
 package com.example.RentalManagementSystem.repository;
 
 import com.example.RentalManagementSystem.entity.Property;
+import com.example.RentalManagementSystem.enums.PropertyStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PropertyRepository extends JpaRepository<Property, Long> {
+    Page<Property> findByStatus(PropertyStatus status, Pageable pageable);
+    Page<Property> findByCityIgnoreCaseContaining(String city, Pageable pageable);
 }
