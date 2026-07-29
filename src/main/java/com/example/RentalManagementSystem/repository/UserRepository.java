6c1e8ae2d0f95ca68595f19ac0102e9b19cda523
Ushudha Sanwa Limbu <ushudhasanwa@gmail.com>
2026-07-29 11:50:17 +0545

    added count methods

--- src/main/java/com/example/RentalManagementSystem/repository/UserRepository.java
+++ src/main/java/com/example/RentalManagementSystem/repository/UserRepository.java
@@ -1,12 +1,20 @@
 package com.example.RentalManagementSystem.repository;
 
 import com.example.RentalManagementSystem.entity.User;
+import com.example.RentalManagementSystem.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
     Optional<User> findByVerificationToken(String token);
+
+    long countByRole(Role role);
+    long countByRoleAndEmailVerifiedTrue(Role role);
+    long countByRoleAndEmailVerifiedFalse(Role role);
+
+    List<User> findTop5ByOrderByCreatedAtDesc();
 }
