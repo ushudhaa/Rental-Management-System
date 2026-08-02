1a86698ff59f2394c4318ec428c508799e516142
Ushudha Sanwa Limbu <ushudhasanwa@gmail.com>
2026-08-02 11:17:55 +0545

    added USER-facing query methods

--- src/main/java/com/example/RentalManagementSystem/repository/PropertyRepository.java
+++ src/main/java/com/example/RentalManagementSystem/repository/PropertyRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+
 public interface PropertyRepository extends JpaRepository<Property, Long> {
     Page<Property> findByStatus(PropertyStatus status, Pageable pageable);
     Page<Property> findByCityIgnoreCaseContaining(String city, Pageable pageable);
