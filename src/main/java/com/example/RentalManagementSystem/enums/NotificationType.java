9e7cfc799df7e9fcf0954c0759115e73f541c2fd
Ushudha Sanwa Limbu <ushudhasanwa@gmail.com>
2026-08-06 11:04:29 +0545

    added new notificationtype enum

---
+++ src/main/java/com/example/RentalManagementSystem/enums/NotificationType.java
@@ -0,0 +1,10 @@
+package com.example.RentalManagementSystem.enums;
+
+public enum NotificationType {
+    LANDLORD_REGISTERED,
+    LANDLORD_VERIFIED,
+    LANDLORD_REJECTED,
+    PAYMENT_RECEIVED,
+    PAYMENT_OVERDUE,
+    GENERAL
+}
